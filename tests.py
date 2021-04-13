e92a93ea06377237b1e338ca9ff0cde90edfb582
Matthew Munson <matthew.munson@uni-hamburg.de>
2021-04-13 09:57:21 +0200

    Failing first XML test should not stop the whole job

--- tests.py
+++ tests.py
@@ -81,6 +81,7 @@ class TestRoutes(CoenoturTests):
                 exceptions.append(E)
         if exceptions != []:
             print('# Not All XML Files Passed\n| File | Error |\n| --- | --- |\n' + '\n'.join(['| {} | {} |'.format(x.filename.split('/')[-1], str(x).split('(')[0]) for x in exceptions]))
+            self.fail()
         else:
             print('# All XML Files Passed')
 
@@ -94,7 +95,7 @@ class TestRoutes(CoenoturTests):
             except SyntaxError as E:
                 exceptions.append(E)
         if exceptions != []:
-            print('#Not All XML Files Passed')
+            print('# Not All XML Files Passed')
             print('| File | Error |\n| :--- | :--- |\n' + '\n'.join(['| {} | {} |'.format(x.filename.split('/')[-1], str(x).split('(')[0]) for x in exceptions]))
             self.fail()
         else:
