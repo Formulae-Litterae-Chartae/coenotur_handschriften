19b7a04ec580a407fc65c22e5794bed59b9ad225
Matthew Munson <matthew.munson@uni-hamburg.de>
2022-04-27 10:40:40 +0200

    New XMLs from Zina and code to deal with them

--- app/auth/email.py
+++ app/auth/email.py
@@ -6,17 +6,17 @@ from app.models import User
 
 def send_password_reset_email(user: User):
     token = user.get_reset_password_token()
-    send_email(_('[Formulae - Litterae - Chartae] Passwort zurücksetzen'),
+    send_email(_('[Coenotur Projekt] Passwort zurücksetzen'),
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
-               text_body=render_template('email/reset_password.txt', user=user, token=token),
-               html_body=render_template('email/reset_password.html', user=user, token=token))
+               text_body=render_template('auth/email_reset_password.txt', user=user, token=token),
+               html_body=render_template('auth/email_reset_password.html', user=user, token=token))
 
 
 def send_email_reset_email(user: User, new_email: str):
     token = user.get_reset_email_token(new_email)
-    send_email(_('[Formulae - Litterae - Chartae] Emailadresse ändern'),
+    send_email(_('[Coenotur Projekt] Emailadresse ändern'),
                sender=current_app.config['ADMINS'][0],
                recipients=[new_email],
-               text_body=render_template('email/reset_email.txt', user=user, token=token),
-               html_body=render_template('email/reset_email.html', user=user, token=token))
+               text_body=render_template('auth/email_reset_email.txt', user=user, token=token),
+               html_body=render_template('auth/email_reset_email.html', user=user, token=token))
